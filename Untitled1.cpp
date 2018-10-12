25eccabf286c411edc990845c29a530e4cd479ab
張軒祥 <mio575446@gmail.com>
2018-10-12 19:32:42 +0800

    First Commit

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -48,94 +48,95 @@ for(i=0;i<r;i++)
 		cin>>matrix[i][j];
 	}
 }
+i=0;j=0;
 for(i=0;i<r;i++)
 {
 	for(j=0;j<c;j++)
 	{
-		while(i==0 && j==0)//matrix[0][0]是最左上角的，只要檢查右和下，以下會把四個角落都做此處理。 
-		{
-			if(matrix[i+1][j]<=matrix[i][j]&&matrix[i][j+1]<=matrix[i][j])//檢查右邊和下面，如果都不大於他那就是peak。 
+	
+	    if(i==0 && j==0){
+		
+		if(matrix[i][j]>=matrix[i+1][j]&&matrix[i][j]>=matrix[i][j+1])//檢查右邊和下面，如果都不大於他那就是peak。 
 			{
 				peaks=peaks+1;//闢可的數目+1 。 
 				peak.morecapacity();//先把裝癖可座標的QUEUE變大。 
-				peak.push(i);peak.push(j);//存入闢可的座標，依序存x,y這樣最後提出來時是按照順序的。			
-			}
-		}
-		while(i==0 &&j==c) //右上角
+				peak.push(i+1);peak.push(j+1);//存入闢可的座標，依序存x,y這樣最後提出來時是按照順序的。			
+			}}
+			else if(i==0 && j==c-1) //右上角
 		{
-			if(matrix[i+1][j]<=matrix[i][j] && matrix[i][j-1]<= matrix[i][j])
+			if(matrix[i][j]>=matrix[i+1][j] && matrix[i][j]>= matrix[i][j-1])
 			{
 				peaks=peaks+1;//闢可的數目+1 。 
 				peak.morecapacity();//先把裝癖可座標的QUEUE變大。 
-				peak.push(i);peak.push(j);//存入闢可的座標，依序存x,y這樣最後提出來時是按照順序的。	
+				peak.push(i+1);peak.push(j+1);//存入闢可的座標，依序存x,y這樣最後提出來時是按照順序的。	
 			}			
 		} 
-		while(i==r && j==0)//左下角。
+		else if(i==r-1 && j==0)//左下角。
 		{
-		    if(matrix[i-1][j]<=matrix[i][j] && matrix[i][j+1]<= matrix[i][j])
+		    if(matrix[i][j]>=matrix[i-1][j] && matrix[i][j]>= matrix[i][j+1])
 			{
 				peaks=peaks+1;//闢可的數目+1 。 
 				peak.morecapacity();//先把裝癖可座標的QUEUE變大。 
-				peak.push(i);peak.push(j);//存入闢可的座標，依序存x,y這樣最後提出來時是按照順序的。	
+				peak.push(i+1);peak.push(j+1);//存入闢可的座標，依序存x,y這樣最後提出來時是按照順序的。	
 			}	
 		}
-		while (i==r && j==c)//右下角。
+		else if (i==r-1 && j==c-1)//右下角。
 		{
-			if(matrix[i-1][j]<=matrix[i][j] && matrix[i][j-1]<= matrix[i][j])
+			if(matrix[i][j]>=matrix[i-1][j] && matrix[i][j]>= matrix[i][j-1])
 			{
 				peaks=peaks+1;//闢可的數目+1 。 
 				peak.morecapacity();//先把裝癖可座標的QUEUE變大。 
-				peak.push(i);peak.push(j);//存入闢可的座標，依序存x,y這樣最後提出來時是按照順序的。	
+				peak.push(i+1);peak.push(j+1);//存入闢可的座標，依序存x,y這樣最後提出來時是按照順序的。	
 			}
 		} 
-		while(i=0 && j!=0 && j!=c)//最上邊，接下來要處理四個邊。
+		else if(i==0 && j!=0 && j!=c-1)//最上邊，接下來要處理四個邊。
 		{
-		if(matrix[i+1][j]<=matrix[i][j] && matrix[i][j-1]<= matrix[i][j] && matrix[i][j+1]<=matrix[i][j])//邊的要比三個，最上邊要比左，右，下。 
+		if(matrix[i][j]>=matrix[i+1][j] && matrix[i][j]>= matrix[i][j-1] && matrix[i][j]>=matrix[i][j+1])//邊的要比三個，最上邊要比左，右，下。 
 			{
 				peaks=peaks+1;//闢可的數目+1 。 
 				peak.morecapacity();//先把裝癖可座標的QUEUE變大。 
-				peak.push(i);peak.push(j);//存入闢可的座標，依序存x,y這樣最後提出來時是按照順序的。	
+				peak.push(i+1);peak.push(j+1);//存入闢可的座標，依序存x,y這樣最後提出來時是按照順序的。	
 			}
 		} 
-		while(i=r && j!=0 && j!=c)//最下邊 
+		else if(i==r-1 && j!=0 && j!=c-1)//最下邊 
 		{
-		if(matrix[i-1][j]<=matrix[i][j] && matrix[i][j-1]<= matrix[i][j] && matrix[i][j+1]<=matrix[i][j])//上，左，右。 
+		if(matrix[i][j]>=matrix[i-1][j] && matrix[i][j]>= matrix[i][j-1] && matrix[i][j]>=matrix[i][j+1])//上，左，右。 
 			{
 				peaks=peaks+1;//闢可的數目+1 。 
 				peak.morecapacity();//先把裝癖可座標的QUEUE變大。 
-				peak.push(i);peak.push(j);//存入闢可的座標，依序存x,y這樣最後提出來時是按照順序的。	
+				peak.push(i+1);peak.push(j+1);//存入闢可的座標，依序存x,y這樣最後提出來時是按照順序的。	
 			}
 		} 
-		while(i!=r && j==0 && i!=0)//最左邊 
+		else if(i!=r-1 && j==0 && i!=0)//最左邊 
 		{
-		if(matrix[i-1][j]<=matrix[i][j] && matrix[i+1][j]<= matrix[i][j] && matrix[i][j+1]<=matrix[i][j])//上，下，右。 
+		if(matrix[i][j]>=matrix[i-1][j] && matrix[i][j]>= matrix[i+1][j] && matrix[i][j]>=matrix[i][j+1])//上，下，右。 
 			{
 				peaks=peaks+1;//闢可的數目+1 。 
 				peak.morecapacity();//先把裝癖可座標的QUEUE變大。 
-				peak.push(i);peak.push(j);//存入闢可的座標，依序存x,y這樣最後提出來時是按照順序的。	
+				peak.push(i+1);peak.push(j+1);//存入闢可的座標，依序存x,y這樣最後提出來時是按照順序的。	
 			}
 		} 
-				while(i!=r && j==c && i!=0)//最右邊 
+				else if(i!=r-1 && j==c-1 && i!=0)//最右邊 
 		{
-		if(matrix[i-1][j]<=matrix[i][j] && matrix[i+1][j]<= matrix[i][j] && matrix[i][j-1]<=matrix[i][j])//上，下，左。 
+		if(matrix[i][j]>=matrix[i-1][j] && matrix[i][j]>= matrix[i+1][j] && matrix[i][j]>=matrix[i][j-1])//上，下，左。 
 			{
 				peaks=peaks+1;//闢可的數目+1 。 
 				peak.morecapacity();//先把裝癖可座標的QUEUE變大。 
-				peak.push(i);peak.push(j);//存入闢可的座標，依序存x,y這樣最後提出來時是按照順序的。	
+				peak.push(i+1);peak.push(j+1);//存入闢可的座標，依序存x,y這樣最後提出來時是按照順序的。	
 			}
 		}
-		while(i!=0&&i!=r&&j!=0&&j!=c)//中間的 
+		else if(i!=0&&i!=r-1&&j!=0&&j!=c-1)//中間的 
 		{
-			if(matrix[i-1][j]<=matrix[i][j] && matrix[i+1][j]<= matrix[i][j] && matrix[i][j-1]<=matrix[i][j]&& matrix[i][j+1]<=matrix[i][j])//上，下，左，右。 
+			if(matrix[i][j]>=matrix[i-1][j] && matrix[i][j]>= matrix[i+1][j] && matrix[i][j]>=matrix[i][j-1]&& matrix[i][j]>=matrix[i][j+1])//上，下，左，右。 
 			{
 				peaks=peaks+1;//闢可的數目+1 。 
 				peak.morecapacity();//先把裝癖可座標的QUEUE變大。 
-				peak.push(i);peak.push(j);//存入闢可的座標，依序存x,y這樣最後提出來時是按照順序的。	
+				peak.push(i+1);peak.push(j+1);//存入闢可的座標，依序存x,y這樣最後提出來時是按照順序的。	
 			}
 		}
-	    } 		 
 	}
-
+	
+}
      cout<<peaks<<endl;
      for(i=0;i<peaks;i++)
      {
@@ -144,5 +145,4 @@ for(i=0;i<r;i++)
 	 }
 	 return 0;
 }
-
 
